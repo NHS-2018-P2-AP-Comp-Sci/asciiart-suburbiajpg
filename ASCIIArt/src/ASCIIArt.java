b3061eb168f95ced2c59f8fe1240d709a3f3c190
s-wue <s-wue@53S50029736.it.bsd405.org>
2018-09-19 08:13:02 -0700

    Turning in project 1!

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -1,21 +1,50 @@
-/**
- * Unit 1 Project
- * 
- * In this project you have the opportunity to create a piece of artwork using ASCII 
- * (American Standard Code for Information Interchange). Using the already created file 
- * named `ASCIIArt.java` write a program that outputs your artwork to the console. 
- * The goal of this project is to use ONLY what we have learned in Unit 1 to create 
- * anything you would like so be creative (and school appropriate). Artwork must also have a caption describing what it's depicting.  
- * 
- * For those of you who have dabbled in coding before, try to contain your code to 
- * just what we have discussed in this unit.  
- * 
- * @author Mr. Rasmussen 
- *
- */
 
 public class ASCIIArt {
-	public static void main (String [] args) {
-		System.out.println("THIS IS MY ASCII ART!!!!");
+	public static void main(String[] args) {
+		makeBoxTop();
+		makeFirstSides();
+		makeBoxSides();
+		makeWordSides();
+		makeBoxSides();
+		makeBoxBottom();
+		makeHandleSides();
+		makeHandleSides();
+		makeHandleSides();
+		makeHandleSides();
+		makeHandleBottom();
 	}
+	
+	public static void makeBoxTop() {
+		System.out.println("______________________________");
+	}
+
+	public static void makeFirstSides() {
+		System.out.println("|   _________                 |");
+		System.out.println("|   |  .--  |                 |");
+		System.out.println("|   |  :..  |       DAYS      |");
+		System.out.println("|   |  \\__) |                 |");
+		System.out.println("|   |_______|                 |");
+	}
+	
+	public static void makeBoxSides() {
+		System.out.println("|                             |");
+	}
+	
+	public static void makeWordSides() {
+		System.out.println("|      S I N C E   O U R      |");
+		System.out.println("|           L A S T           |");
+		System.out.println("|       N O N S E N S E       |");
+	}
+	
+	public static void makeBoxBottom() {
+		System.out.println("|_____________________________|");
+	}
+	
+	public static void makeHandleSides() {
+		System.out.println("             \"  \" ");
+	}
+	
+	public static void makeHandleBottom() {
+		System.out.println("             \"__\" ");
+	}	
 }
